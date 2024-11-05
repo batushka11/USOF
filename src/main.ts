6ebd4a778b4d7afe8ce856d0e02893ec9db7ae38
Andrii Zhupanov <Andrii@MacBook-Pro-Andrey.local>
2024-11-05 15:37:05 +0100

    change docs URL

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
 		.build()
 
 	const document = SwaggerModule.createDocument(app, config)
-	SwaggerModule.setup('/api/docs', app, document)
+	SwaggerModule.setup('/docs', app, document)
 	await app.listen(4200)
 }
 bootstrap()
